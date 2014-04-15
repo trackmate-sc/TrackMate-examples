c5e3c5bc4fd9c0ce85fd9fed5da9b1271d41e423
Jean-Yves Tinevez <jean-yves.tinevez@pasteur.fr>
2014-04-15 19:03:35 +0200

    WIP: example of a custom tracker.
    Signed-off-by: Jean-Yves Tinevez <jean-yves.tinevez@pasteur.fr>

---
+++ src/main/java/plugin/trackmate/examples/tracker/RandomLinkingTracker.java
@@ -0,0 +1,139 @@
+package plugin.trackmate.examples.tracker;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.NavigableSet;
+import java.util.Random;
+import java.util.Set;
+
+import org.jgrapht.graph.DefaultWeightedEdge;
+import org.jgrapht.graph.SimpleWeightedGraph;
+
+import fiji.plugin.trackmate.Logger;
+import fiji.plugin.trackmate.Spot;
+import fiji.plugin.trackmate.SpotCollection;
+import fiji.plugin.trackmate.tracking.SpotTracker;
+
+public class RandomLinkingTracker implements SpotTracker
+{
+
+	/**
+	 * This is how we return tracking results: as a JGraphT graph.
+	 */
+	private SimpleWeightedGraph< Spot, DefaultWeightedEdge > graph;
+
+	private final SpotCollection sc;
+
+	private final int maxLinks;
+
+	private String errorMessage;
+
+	public RandomLinkingTracker( final SpotCollection spots, final int maxLinks )
+	{
+		this.sc = spots;
+		this.maxLinks = maxLinks;
+	}
+
+	@Override
+	public SimpleWeightedGraph< Spot, DefaultWeightedEdge > getResult()
+	{
+		return graph;
+	}
+
+	@Override
+	public boolean checkInput()
+	{
+		if ( sc == null )
+		{
+			errorMessage = "SpotCollection is null.\n";
+			return false;
+		}
+
+		if ( maxLinks < 0 )
+		{
+			errorMessage = "Max links per frame is negative.\n";
+			return false;
+		}
+
+		return true;
+	}
+
+	@Override
+	public boolean process()
+	{
+		final Random ran = new Random();
+		// First create a new empty graph.
+		graph = new SimpleWeightedGraph< Spot, DefaultWeightedEdge >( DefaultWeightedEdge.class );
+
+		// Loop over frames
+		final NavigableSet< Integer > frames = sc.keySet();
+		Set< Spot > previousSpots = null;
+
+		for ( final Integer frame : frames )
+		{
+			if ( null == previousSpots )
+			{
+				// Collect some spots.
+				previousSpots = pickNRandomSpot( frame );
+				// And loop to next frame.
+				continue;
+			}
+
+			final Set< Spot > targetSpots = pickNRandomSpot( frame );
+			final Iterator< Spot > itp = previousSpots.iterator();
+			final Iterator< Spot > itt = targetSpots.iterator();
+			while ( itp.hasNext() && itt.hasNext() )
+			{
+				// Create link between 2 random spots.
+				final DefaultWeightedEdge edge = graph.addEdge( itp.next(), itt.next() );
+				// Give a random weight.
+				graph.setEdgeWeight( edge, Math.abs( ran.nextDouble() ) );
+			}
+
+			previousSpots = targetSpots;
+		}
+
+		return true;
+	}
+
+	private final Set< Spot > pickNRandomSpot( final int frame )
+	{
+		// We only play with spots marked as visible.
+		final int nSpots = sc.getNSpots( frame, true );
+
+		// Collect them.
+		final List< Spot > spots = new ArrayList< Spot >( nSpots );
+		for ( final Spot spot : sc.iterable( frame, true ) )
+		{
+			spots.add( spot );
+		}
+
+		// Shuffle them
+		Collections.shuffle( spots );
+
+		// Pick the first ones
+		final Set< Spot > randomSpots = new HashSet< Spot >( maxLinks );
+		final int n = Math.min( maxLinks, nSpots );
+		for ( int i = 0; i < n; i++ )
+		{
+			randomSpots.add( spots.get( i ) );
+		}
+		return randomSpots;
+	}
+
+	@Override
+	public String getErrorMessage()
+	{
+		return errorMessage;
+	}
+
+	@Override
+	public void setLogger( final Logger logger )
+	{
+		// Ignored.
+	}
+
+}
---
+++ src/main/java/plugin/trackmate/examples/tracker/RandomLinkingTrackerFactory.java
@@ -0,0 +1,179 @@
+package plugin.trackmate.examples.tracker;
+
+import ij.ImageJ;
+import ij.ImagePlus;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.swing.ImageIcon;
+
+import org.jdom2.DataConversionException;
+import org.jdom2.Element;
+import org.scijava.plugin.Plugin;
+
+import fiji.plugin.trackmate.Model;
+import fiji.plugin.trackmate.SpotCollection;
+import fiji.plugin.trackmate.TrackMatePlugIn_;
+import fiji.plugin.trackmate.gui.ConfigurationPanel;
+import fiji.plugin.trackmate.tracking.SpotTracker;
+import fiji.plugin.trackmate.tracking.SpotTrackerFactory;
+
+@Plugin( type = SpotTrackerFactory.class )
+public class RandomLinkingTrackerFactory implements SpotTrackerFactory
+{
+
+	private static final String INFO_TEXT = "<html>This tracker is the worst: It links particle at random.</html>";
+
+	private static final String KEY = "RANDOM_LINKING_TRACKER";
+
+	private static final String NAME = "Random linking tracker";
+
+	static final String MAX_LINKS_PER_FRAME = "MAX_LINKS_PER_FRAME";
+
+	static final int DEFAULT_MAX_LINKS_PER_FRAME = 5;
+
+	private String errorMessage;
+
+	@Override
+	public String getInfoText()
+	{
+		return INFO_TEXT;
+	}
+
+	@Override
+	public ImageIcon getIcon()
+	{
+		return null;
+	}
+
+	@Override
+	public String getKey()
+	{
+		return KEY;
+	}
+
+	@Override
+	public String getName()
+	{
+		return NAME;
+	}
+
+	@Override
+	public SpotTracker create( final SpotCollection spots, final Map< String, Object > settings )
+	{
+		final int maxLinks = ( Integer ) settings.get( MAX_LINKS_PER_FRAME );
+		return new RandomLinkingTracker( spots, maxLinks );
+	}
+
+	@Override
+	public ConfigurationPanel getTrackerConfigurationPanel( final Model model )
+	{
+		return new RandomLinkingTrackerConfigPanel();
+	}
+
+	@Override
+	public boolean marshall( final Map< String, Object > settings, final Element element )
+	{
+		if ( !checkSettingsValidity( settings ) ) { return false; }
+
+		/*
+		 * To save to XML, we simply add an attribute to the element, using the
+		 * key as a XML tag.
+		 */
+		final int val = ( Integer ) settings.get( MAX_LINKS_PER_FRAME );
+		element.setAttribute( MAX_LINKS_PER_FRAME, "" + val );
+		return true;
+	}
+
+	@Override
+	public boolean unmarshall( final Element element, final Map< String, Object > settings )
+	{
+		try
+		{
+			final int val = element.getAttribute( MAX_LINKS_PER_FRAME ).getIntValue();
+			settings.put( MAX_LINKS_PER_FRAME, val );
+		}
+		catch ( final DataConversionException e )
+		{
+			errorMessage = "Could not read " + MAX_LINKS_PER_FRAME + " attribute:\n" + e.getMessage();
+			return false;
+		}
+		return true;
+	}
+
+	@Override
+	public String toString( final Map< String, Object > sm )
+	{
+		// If the settings is not properly built, we return the error message.
+		if ( !checkSettingsValidity( sm ) ) { return errorMessage; }
+
+		final StringBuilder str = new StringBuilder();
+		final int nLinks = ( Integer ) sm.get( MAX_LINKS_PER_FRAME );
+		str.append( "  Max number of links per frame pair: " + nLinks + ".\n" );
+
+		return str.toString();
+	}
+
+	@Override
+	public Map< String, Object > getDefaultSettings()
+	{
+		// Just made of 1 parameter.
+		final Map< String, Object > settings = new HashMap< String, Object >( 1 );
+		settings.put( MAX_LINKS_PER_FRAME, DEFAULT_MAX_LINKS_PER_FRAME );
+		return settings;
+	}
+
+	@Override
+	public boolean checkSettingsValidity( final Map< String, Object > settings )
+	{
+		// Be ULTRA pedantic.
+
+		if ( null == settings )
+		{
+			errorMessage = "Settings map is null.\n";
+			return false;
+		}
+
+		if ( settings.size() != 1 )
+		{
+			errorMessage = "Settings map should contain only 1 key, found " + settings.size() + "./n";
+			return false;
+		}
+
+		final String key = settings.keySet().iterator().next();
+		if ( !key.equals( MAX_LINKS_PER_FRAME )) {
+			errorMessage = "Could not find parameter " + MAX_LINKS_PER_FRAME + " in settings map.\n";
+			return false;
+		}
+
+		final Object obj = settings.get( key );
+		if ( !Integer.class.isInstance( obj ) )
+		{
+			errorMessage = "Value for parameter " + MAX_LINKS_PER_FRAME + " should be of Integer class, but found " + obj.getClass() + "./n";
+			return false;
+		}
+
+		final int val = ( Integer ) obj;
+		if (val < 0) {
+			errorMessage = "Found a negative value for " + MAX_LINKS_PER_FRAME + ".\n";
+			return false;
+		}
+		// All of this for just one parameter...
+		return true;
+	}
+
+	@Override
+	public String getErrorMessage()
+	{
+		return errorMessage;
+	}
+
+	public static void main( final String[] args )
+	{
+		ImageJ.main( args );
+		new ImagePlus( "../fiji/samples/FakeTracks.tif" ).show();
+		new TrackMatePlugIn_().run( "" );
+	}
+
+}
